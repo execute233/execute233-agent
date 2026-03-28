6784e4f20828521ac749558a3ab2c760a5854f6b
execute233 <1647643661@qq.com>
2026-03-28 21:29:28 +0800

    init

---
+++ src/main/java/com/execute233/agent/AgentApplication.java
@@ -0,0 +1,18 @@
+package com.execute233.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author execute233
+ * @date 2026/3/27
+ **/
+@SpringBootApplication
+public class AgentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgentApplication.class);
+    }
+
+}
